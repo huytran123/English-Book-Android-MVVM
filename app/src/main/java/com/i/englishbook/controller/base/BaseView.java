0f32b597a5af9dfaa11c0dc49f981a489ac63891
Huy Tran <huy.hoang2225@gmail.com>
2017-09-27 22:55:03 +0700

    HuyTran
    Add tool bar to detail activity

--- app/src/main/java/com/i/englishbook/controller/base/BaseView.java
+++ app/src/main/java/com/i/englishbook/controller/base/BaseView.java
@@ -1,5 +1,7 @@
 package com.i.englishbook.controller.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,7 +10,13 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BaseView extends AppCompatActivity {
-    public void showMess(String mess){
+    public void showMess(String mess) {
         new AlertDialog.Builder(this).setMessage(mess).show();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
     }
 }
